a9901a4d751ddd76792cf975baa062a7db0b0f8e
anamaria1299 <ana.rincon-c@mail.escuelaing.edu.co>
2019-03-10 21:15:27 -0500

    First commit

---
+++ src/main/java/edu/eci/arsw/GBoard/GBoardApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.GBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/GBoard/GBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.eci.arsw.GBoard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
