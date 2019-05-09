848819ae70610499ebaab433a564678cbbb87b6d
Santiago-Rocha <santiago.rocha@tulep.com>
2019-05-09 14:59:39 -0500

    add extra function to board

--- src/main/resources/static/js/apiBoard.js
+++ src/main/resources/static/js/apiBoard.js
@@ -1,5 +1,6 @@
 apiBoard = (function () {
-  var $ = function (id) { return document.getElementById(id) };
+  var $$ = function (id) { return document.getElementById(id) };
+  var SocketFunction = null;
 
   //Initialize Canvas
   var canvas = this.__canvas = new fabric.Canvas('c', {
@@ -9,15 +10,19 @@ apiBoard = (function () {
 
   fabric.Object.prototype.transparentCorners = false;
 
-  var drawingModeEl = $('drawing-mode'),
-    drawingOptionsEl = $('drawing-mode-options'),
-    drawingColorEl = $('drawing-color'),
-    drawingLineWidthEl = $('drawing-line-width'),
-    clearEl = $('clear-canvas'),
-    drawingMoveEl = $('drawing-move'),
-    drawTriangleEl = $('drawing-triangle'),
-    drawSquareEl = $('drawing-square'),
-    drawCircleEl = $('drawing-circle');
+  var drawingModeEl = $$('drawing-mode'),
+    drawingOptionsEl = $$('drawing-mode-options'),
+    drawingColorEl = $$('drawing-color'),
+    drawingLineWidthEl = $$('drawing-line-width'),
+    clearEl = $$('clear-canvas'),
+    drawingMoveEl = $$('drawing-move'),
+    drawTriangleEl = $$('drawing-triangle'),
+    drawSquareEl = $$('drawing-square'),
+    drawCircleEl = $$('drawing-circle'),
+    drawTextEl = $$('drawing-text'),
+    drawImageEl = $$('drawing-image'),
+    drawUpload = $$('drawing-upload'),
+    drawDownload = $$('drawig-download');
 
   clearEl.onclick = function () { canvas.clear() };
 
@@ -54,6 +59,40 @@ apiBoard = (function () {
     }
   }
 
+  drawImageEl.onclick = function () {
+    canvas.isDrawingMode = false;
+    if($("#contentImg")) $("#contentImg").remove();
+    $("body").append('<div id="contentImg" style="display:none"><input type="file" id="inputImg"><img src="" id="my-image" height="200" alt="Image preview..."></div>')
+    $("#inputImg").click();
+
+    $('#inputImg').change(function (e) {
+      var imgElement = document.querySelector('img');
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      reader.onloadend = function () {
+        imgElement.src = reader.result;
+        var img = $$('my-image');
+        canvas.add(new fabric.Image(img, { left: 0, top: 0, scaleX: 0.20 ,scaleY: 0.20 }));
+      }
+
+      if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+      } else {
+        imgElement.src = "";
+      }
+    });
+
+
+  }
+
+  drawUpload.onclick = function(){
+    upload();
+  }
+
+  drawDownload.onclick = function(){
+    download();
+  }
+
 
 
   if (fabric.PatternBrush) {
@@ -81,13 +120,16 @@ apiBoard = (function () {
     if (canvas.shape) {
       console.log(options.e.clientY, options.e.clientX);
       var shp = canvas.shape(options.e.clientX - 15, options.e.clientY - 115);
-      canvas.add(shp).setActiveObject(shp);
-      canvas.shape = null;
+      if (shp) {
+        canvas.add(shp).setActiveObject(shp);
+        canvas.shape = null;
+      }
     }
   });
 
   var Socekt = function (f) {
-    canvas.on('mouse:up', f);
+    SocketFunction =f 
+    canvas.on('mouse:up', SocketFunction);
   }
 
   //public methods
@@ -104,6 +146,33 @@ apiBoard = (function () {
     canvas.calcOffset();
   }
 
+  var download = function(){
+		var file = $("<a />", {
+		    "download": "data.json",
+		    "href" : "data:application/json;charset=utf-8," + encodeURIComponent(getJson()),
+		  });
+		file[0].click();
+	}
+	
+	var upload = function(){
+		$("body").append('<input id="inputUpload" type="file" style="display:none">');
+		$("#inputUpload").click();
+		var fr = new FileReader();
+		  
+		  fr.onload = function(e) { 
+		    var result = JSON.parse(e.target.result);
+		    var formatted = JSON.stringify(result, null, 2);
+        loadJson(formatted);
+        SocketFunction();
+		    $("#inputUpload").remove();
+		  }
+		
+		$("#inputUpload").change(function(){
+			fr.readAsText($("#inputUpload").prop('files').item(0));
+    });
+  }
+		 
+
   return {
     getJson: getJson,
     loadJson: loadJson,
@@ -114,10 +183,11 @@ apiBoard = (function () {
 
 })();
 
-window.onload = function () {
+$(document).ready(function(){
   $("canvas").css("width", "100%");
   apiBoard.reSize($("#boardContainer").width());
-};
+});
+
 
 $(window).resize(function () {
   apiBoard.reSize($("#boardContainer").width());
